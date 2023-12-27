10bdbb7cee0aa1c1e8cf802e6e610e98d102a1e6
George <georgedreemer@proton.me>
2023-12-27 18:45:51 +0100

    Completed Chapter 3, Assessing model fit

--- code/Data Scientist Professional with Python/Introduction to Regression with statsmodels in Python.py
+++ code/Data Scientist Professional with Python/Introduction to Regression with statsmodels in Python.py
@@ -387,65 +387,105 @@ plt.show()
 
 ### --- Exercise 1 --- ###
 
+# Print a summary of mdl_click_vs_impression_orig
+print(mdl_click_vs_impression_orig.summary())
 
+# Print a summary of mdl_click_vs_impression_trans
+print(mdl_click_vs_impression_trans.summary())
 
+# Print the coeff of determination for mdl_click_vs_impression_orig
+print(mdl_click_vs_impression_orig.rsquared)
 
-### --- Exercise 2 --- ###
-
-
-
-
-### --- Exercise 3 --- ###
-
-
-
-
-### --- Exercise 4 --- ###
-
-
-
-
-### --- Exercise 5 --- ###
-
-
-
-
-### --- Exercise 6 --- ###
-
-
-
-
-### --- Exercise 7 --- ###
-
-
+# Print the coeff of determination for mdl_click_vs_impression_trans
+print(mdl_click_vs_impression_trans.rsquared)
 
 
 ### --- Exercise 2 --- ###
 
+# Calculate mse_orig for mdl_click_vs_impression_orig
+mse_orig = mdl_click_vs_impression_orig.mse_resid
 
+# Calculate rse_orig for mdl_click_vs_impression_orig and print it
+rse_orig = np.sqrt(mse_orig)
+print("RSE of original model: ", rse_orig)
+
+# Calculate mse_trans for mdl_click_vs_impression_trans
+mse_trans = mdl_click_vs_impression_trans.mse_resid
+
+# Calculate rse_trans for mdl_click_vs_impression_trans and print it
+rse_trans = np.sqrt(mse_trans)
+print("RSE of transformed model: ", rse_trans)
 
 
 ### --- Exercise 3 --- ###
 
+# Plot the residuals vs. fitted values
+sns.residplot(x="n_convenience", y="price_twd_msq", data=taiwan_real_estate, lowess=True)
+plt.xlabel("Fitted values")
+plt.ylabel("Residuals")
 
+# Show the plot
+plt.show()
 
 
 ### --- Exercise 4 --- ###
 
+# Import qqplot
+from statsmodels.api import qqplot
 
+# Create the Q-Q plot of the residuals
+qqplot(data=mdl_price_vs_conv.resid, fit=True, line="45")
+
+# Show the plot
+plt.show()
 
 
 ### --- Exercise 5 --- ###
 
+# Preprocessing steps
+model_norm_residuals = mdl_price_vs_conv.get_influence().resid_studentized_internal
+model_norm_residuals_abs_sqrt = np.sqrt(np.abs(model_norm_residuals))
 
+# Create the scale-location plot
+sns.regplot(x=mdl_price_vs_conv.fittedvalues, y=model_norm_residuals_abs_sqrt, ci=None, lowess=True)
+plt.xlabel("Fitted values")
+plt.ylabel("Sqrt of abs val of stdized residuals")
+
+# Show the plot
+plt.show()
 
 
 ### --- Exercise 6 --- ###
 
+# Create summary_info
+summary_info = mdl_price_vs_dist.get_influence().summary_frame()
 
+# Add the hat_diag column to taiwan_real_estate, name it leverage
+taiwan_real_estate["leverage"] = summary_info['hat_diag']
+
+# Sort taiwan_real_estate by leverage in descending order and print the head
+print(taiwan_real_estate.sort_values(by='leverage', ascending = False).head())
 
 
 ### --- Exercise 7 --- ###
+
+# Create summary_info
+summary_info = mdl_price_vs_dist.get_influence().summary_frame()
+
+# Add the hat_diag column to taiwan_real_estate, name it leverage
+taiwan_real_estate["leverage"] = summary_info["hat_diag"]
+
+# Add the cooks_d column to taiwan_real_estate, name it cooks_dist
+taiwan_real_estate["cooks_dist"] = summary_info["cooks_d"]
+
+# Sort taiwan_real_estate by cooks_dist in descending order and print the head.
+print(taiwan_real_estate.sort_values(by='cooks_dist', ascending=False).head())
+
+
+
+## Chapter 4
+
+### --- Exercise 1 --- ###
 
 
 
