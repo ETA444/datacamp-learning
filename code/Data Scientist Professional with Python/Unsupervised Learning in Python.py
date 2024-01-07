4afecae0ed512017cc41aa791f2d6b58b47174e7
George <georgedreemer@proton.me>
2024-01-07 15:51:28 +0100

    Completed Chapter 2, Visualization with Hierarchical Clustering and t-SNE

--- code/Data Scientist Professional with Python/Unsupervised Learning in Python.py
+++ code/Data Scientist Professional with Python/Unsupervised Learning in Python.py
@@ -174,21 +174,97 @@ print(df.sort_values('labels'))
 
 ### --- Exercise 1 --- ###
 
+# Perform the necessary imports
+from scipy.cluster.hierarchy import linkage, dendrogram
+import matplotlib.pyplot as plt
+
+# Calculate the linkage: mergings
+mergings = linkage(
+            samples,
+            method='complete'
+)
+
+# Plot the dendrogram, using varieties as labels
+dendrogram(mergings,
+           labels=varieties,
+           leaf_rotation=90,
+           leaf_font_size=6,
+)
+plt.show()
 
 
 
 ### --- Exercise 2 --- ###
 
+# Import normalize
+from sklearn.preprocessing import normalize
 
+# Normalize the movements: normalized_movements
+normalized_movements = normalize(movements)
+
+# Calculate the linkage: mergings
+mergings = linkage(
+                normalized_movements,
+                method='complete'
+)
+
+# Plot the dendrogram
+dendrogram(
+            mergings,
+            labels=companies,
+            leaf_rotation=90,
+            leaf_font_size=6
+)
+plt.show()
 
 
 ### --- Exercise 3 --- ###
 
+# Perform the necessary imports
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import linkage, dendrogram
+
+# Calculate the linkage: mergings
+mergings = linkage(
+                samples,
+                method='single'
+)
+
+# Plot the dendrogram
+dendrogram(
+            mergings,
+            labels=country_names,
+            leaf_rotation=90,
+            leaf_font_size=6
+)
+plt.show()
 
 
 
 ### --- Exercise 4 --- ###
 
+# Perform the necessary imports
+import pandas as pd
+from scipy.cluster.hierarchy import fcluster
+
+# Use fcluster to extract labels: labels
+labels = fcluster(
+                mergings,
+                t=6,
+                criterion='distance'
+)
+
+# Create a DataFrame with labels and varieties as columns: df
+df = pd.DataFrame({'labels': labels, 'varieties': varieties})
+
+# Create crosstab: ct
+ct = pd.crosstab(
+    df['labels'],
+    df['varieties']
+)
+
+# Display ct
+print(ct)
 
 
 
@@ -199,16 +275,54 @@ print(df.sort_values('labels'))
 
 ### --- Exercise 6 --- ###
 
+# Import TSNE
+from sklearn.manifold import TSNE
 
+# Create a TSNE instance: model
+model = TSNE(learning_rate=200)
+
+# Apply fit_transform to samples: tsne_features
+tsne_features = model.fit_transform(samples)
+
+# Select the 0th feature: xs
+xs = tsne_features[:,0]
+
+# Select the 1st feature: ys
+ys = tsne_features[:,1]
+
+# Scatter plot, coloring by variety_numbers
+plt.scatter(xs,ys,c=variety_numbers)
+plt.show()
 
 
 ### --- Exercise 7 --- ###
 
+# Import TSNE
+from sklearn.manifold import TSNE
 
+# Create a TSNE instance: model
+model = TSNE(learning_rate=50)
 
+# Apply fit_transform to normalized_movements: tsne_features
+tsne_features = model.fit_transform(normalized_movements)
 
-### --- Exercise 8 --- ###
+# Select the 0th feature: xs
+xs = tsne_features[:,0]
 
+# Select the 1th feature: ys
+ys = tsne_features[:,1]
+
+# Scatter plot
+plt.scatter(
+    xs,
+    ys,
+    alpha=0.5
+)
+
+# Annotate the points
+for x, y, company in zip(xs, ys, companies):
+    plt.annotate(company, (x, y), fontsize=5, alpha=0.75)
+plt.show()
 
 
 
@@ -216,12 +330,55 @@ print(df.sort_values('labels'))
 
 ### --- Exercise 1 --- ###
 
+# Perform the necessary imports
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 
+# Assign the 0th column of grains: width
+width = grains[:,0]
+
+# Assign the 1st column of grains: length
+length = grains[:,1]
+
+# Scatter plot width vs length
+plt.scatter(width, length)
+plt.axis('equal')
+plt.show()
+
+# Calculate the Pearson correlation
+correlation, pvalue = pearsonr(width, length)
+
+# Display the correlation
+print(correlation)
 
 
 ### --- Exercise 2 --- ###
 
+# Import PCA
+from sklearn.decomposition import PCA
 
+# Create PCA instance: model
+model = PCA()
+
+# Apply the fit_transform method of model to grains: pca_features
+pca_features = model.fit_transform(grains)
+
+# Assign 0th column of pca_features: xs
+xs = pca_features[:,0]
+
+# Assign 1st column of pca_features: ys
+ys = pca_features[:,1]
+
+# Scatter plot xs vs ys
+plt.scatter(xs, ys)
+plt.axis('equal')
+plt.show()
+
+# Calculate the Pearson correlation of xs and ys
+correlation, pvalue = pearsonr(xs, ys)
+
+# Display the correlation
+print(correlation)
 
 
 ### --- Exercise 3 --- ###
